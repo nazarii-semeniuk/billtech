470070cf1f2259f7bb8eda72b8111fe457a3d4f0
Nazarii Semeniuk <qnazar.semenyuk@gmail.com>
2023-09-21 13:25:40 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logo">
+    <img src="/images/logo.svg" alt="Logo">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
